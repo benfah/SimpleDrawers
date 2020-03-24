656773c5f1efb12da75542c87751e2493c064fa4
benfah <vayegame@gmail.com>
2020-03-24 18:38:07 +0100

    See desc.
    - Removed unnecessary imports
    - Added better drawer inventory handler
    - Added finished drawer controller logic

---
+++ src/main/java/me/benfah/simpledrawers/block/BlockDrawerController.java
@@ -0,0 +1,63 @@
+package me.benfah.simpledrawers.block;
+
+import me.benfah.simpledrawers.block.entity.BlockEntityDrawerController;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager.Builder;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+
+public class BlockDrawerController extends BlockWithEntity
+{
+	
+	public static DirectionProperty FACING = HorizontalFacingBlock.FACING;
+	
+	public BlockDrawerController(Settings settings)
+	{
+		super(settings);
+		this.setDefaultState(getDefaultState().with(FACING, Direction.NORTH));
+	}
+	
+	@Override
+	public BlockState rotate(BlockState state, BlockRotation rotation)
+	{
+		return state.with(FACING, rotation.rotate(state.get(FACING)));
+	}
+	
+	@Override
+	protected void appendProperties(Builder<Block, BlockState> builder)
+	{
+		builder.add(FACING);
+	}
+	
+	public BlockState mirror(BlockState state, BlockMirror mirror)
+	{
+		return state.rotate(mirror.getRotation(state.get(FACING)));
+	}
+
+	public BlockState getPlacementState(ItemPlacementContext ctx)
+	{
+		return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+	}
+	
+	@Override
+	public BlockRenderType getRenderType(BlockState state)
+	{
+		return BlockRenderType.MODEL;
+	}
+	
+	@Override
+	public BlockEntity createBlockEntity(BlockView view)
+	{
+		return new BlockEntityDrawerController();
+	}
+	
+}
---
+++ src/main/java/me/benfah/simpledrawers/block/entity/BlockEntityDrawerController.java
@@ -0,0 +1,156 @@
+package me.benfah.simpledrawers.block.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.benfah.simpledrawers.block.BlockBasicDrawer;
+import me.benfah.simpledrawers.block.BlockDrawerController;
+import me.benfah.simpledrawers.block.entity.holder.InventoryHandler;
+import me.benfah.simpledrawers.init.SDBlockEntities;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.SidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+public class BlockEntityDrawerController extends BlockEntity implements SidedInventory
+{
+
+	List<BlockPos> drawerPositions = new ArrayList<>();
+
+	public BlockEntityDrawerController()
+	{
+		super(SDBlockEntities.DRAWER_CONTROLLER);
+	}
+
+	
+	public void updateDrawerPositions()
+	{
+		drawerPositions.clear();
+		Direction d = getCachedState().get(BlockDrawerController.FACING).getOpposite();
+		BlockPos pos = getPos().offset(d);
+		for (int offsetY = -1; offsetY <= 1; offsetY++)
+		{
+			for (int offset = -1; offset <= 1; offset++)
+			{
+				BlockPos drawerPos;
+				if (d == Direction.NORTH || d == Direction.SOUTH)
+					drawerPos = new BlockPos(pos.getX() + offset, pos.getY() + offsetY, pos.getZ());
+				else
+					drawerPos = new BlockPos(pos.getX(), pos.getY() + offsetY, pos.getZ() + offset);
+
+				if (getWorld().getBlockState(drawerPos).getBlock() instanceof BlockBasicDrawer)
+				{
+					drawerPositions.add(drawerPos);
+				}
+
+			}
+		}
+	}
+	
+	private List<BlockPos> getDrawerPositions()
+	{
+		updateDrawerPositions();
+		return drawerPositions;
+	}
+	
+	@Override
+	public int getInvSize()
+	{
+		updateDrawerPositions();
+		return getDrawerPositions().size() * 2;
+	}
+
+	@Override
+	public boolean isInvEmpty()
+	{
+		return getDrawerPositions().stream()
+				.allMatch((drawerPos) -> getInventoryHandler(drawerPos).isInvEmpty());
+	}
+
+	@Override
+	public ItemStack getInvStack(int slot)
+	{
+		return getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).getInvStack(getDrawerPosition(slot));
+	}
+
+	@Override
+	public ItemStack takeInvStack(int slot, int amount)
+	{
+		return getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).takeInvStack(getDrawerPosition(slot), amount);
+	}
+
+	@Override
+	public ItemStack removeInvStack(int slot)
+	{
+		return getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).removeInvStack(getDrawerPosition(slot));
+	}
+
+	@Override
+	public void setInvStack(int slot, ItemStack stack)
+	{
+		getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).setInvStack(getDrawerPosition(slot), stack);
+	}
+
+	@Override
+	public boolean canPlayerUseInv(PlayerEntity player)
+	{
+		return false;
+	}
+
+	@Override
+	public void clear()
+	{
+		getDrawerPositions().forEach((drawerPos) -> getInventoryHandler(drawerPos).clear());
+	}
+
+	@Override
+	public int[] getInvAvailableSlots(Direction side)
+	{
+		int[] availableSlots = new int[getDrawerPositions().size() * 2];
+		
+		for(int i = 0; i < getDrawerPositions().size() * 2; i++)
+		{
+			availableSlots[i] = i;
+		}
+		
+		return availableSlots;
+	}
+
+	@Override
+	public boolean canInsertInvStack(int slot, ItemStack stack, Direction dir)
+	{
+		return getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).canInsertInvStack(getDrawerPosition(slot), stack, dir);
+	}
+
+	@Override
+	public boolean canExtractInvStack(int slot, ItemStack stack, Direction dir)
+	{
+		return getInventoryHandler(getDrawerPositions().get(getListPosition(slot))).canExtractInvStack(getDrawerPosition(slot), stack, dir);
+	}
+	
+	private InventoryHandler getInventoryHandler(BlockPos pos)
+	{
+		return ((BlockEntityBasicDrawer) world.getBlockEntity(pos)).getHolder().getInventoryHandler();
+	}
+	
+	private int getListPosition(int slot)
+	{
+		if(slot < getDrawerPositions().size())
+		return slot;
+		return slot - getDrawerPositions().size();
+	}
+	private int getDrawerPosition(int slot)
+	{
+		return slot / getDrawerPositions().size();
+	}
+	
+	@Override
+	public void markDirty()
+	{
+		getDrawerPositions().forEach((drawerPos) -> getInventoryHandler(drawerPos).markDirty());
+		super.markDirty();
+	}
+	
+}
