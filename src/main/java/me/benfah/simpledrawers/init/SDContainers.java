98575382b897650305e2b8cf97dd90192e9fbca7
benfah <vayegame@gmail.com>
2020-04-20 13:15:03 +0200

    Updated mappings, fixed typo

--- src/main/java/me/benfah/simpledrawers/init/SDContainers.java
+++ src/main/java/me/benfah/simpledrawers/init/SDContainers.java
@@ -58,7 +58,7 @@ public class SDContainers
 
 	public static void initClient()
 	{
-		ScreenProviderRegistry.INSTANCE.registerFactory(BASIC_DRAWER_CONTAINER, (container) -> new BasicDrawerContainerScreen((DrawerContainer<?>) container, new Identifier(SimpleDrawersMod.MOD_ID, "tatures/gui/drawer_1.png")));
+		ScreenProviderRegistry.INSTANCE.registerFactory(BASIC_DRAWER_CONTAINER, (container) -> new BasicDrawerContainerScreen((DrawerContainer<?>) container, new Identifier(SimpleDrawersMod.MOD_ID, "textures/gui/drawer_1.png")));
 		ScreenProviderRegistry.INSTANCE.registerFactory(DOUBLE_DRAWER_CONTAINER, (container) -> new BasicDrawerContainerScreen((DrawerContainer<?>) container, new Identifier(SimpleDrawersMod.MOD_ID, "textures/gui/drawer_2.png")));
 		ScreenProviderRegistry.INSTANCE.registerFactory(QUAD_DRAWER_CONTAINER, (container) -> new BasicDrawerContainerScreen((DrawerContainer<?>) container, new Identifier(SimpleDrawersMod.MOD_ID, "textures/gui/drawer_4.png")));
 
